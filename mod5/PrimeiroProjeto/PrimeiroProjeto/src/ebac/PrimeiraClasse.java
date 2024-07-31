218ce4e52c98bb0c14e6ec7baac568eeefa8eb51
Tiago Molero <tiagomolero@gmail.com>
2024-07-30 22:26:52 -0300

    Commit do Primeiro Projeto - MD5

---
+++ mod5/PrimeiroProjeto/PrimeiroProjeto/src/ebac/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package ebac;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Tiago Molero");
+	}
+
+}
