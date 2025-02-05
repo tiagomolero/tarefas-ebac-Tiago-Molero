5513ed9ed8a30b5c349c59d341ee5d5b39e65d74
Tiago Molero <tiagomolero@gmail.com>
2025-02-05 20:47:35 -0300

    @Before - método que é iniciado antes dos test

--- mod25/ProjetoSistemaVendas/test/br/com/molero/ClienteServiceTest.java
+++ mod25/ProjetoSistemaVendas/test/br/com/molero/ClienteServiceTest.java
@@ -1,36 +1,39 @@
 package br.com.molero;
 
-import br.com.molero.dao.ClienteDAO;
 import br.com.molero.dao.IClienteDAO;
 import br.com.molero.domain.Cliente;
 import br.com.molero.mock.ClienteDAOMock;
 import br.com.molero.services.ClienteService;
 import br.com.molero.services.IClienteService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class ClienteTest {
+public class ClienteServiceTest {
     private IClienteService iClienteService;
+    private Cliente cliente;
 
-    public ClienteTest(){
+    public ClienteServiceTest(){
         IClienteDAO iClienteDAO = new ClienteDAOMock();
         iClienteService = new ClienteService(iClienteDAO);
     }
 
-    @Test
-    public void buscarCliente(){
-        Cliente cliente = new Cliente();
+    @Before
+    public void init(){
+        cliente = new Cliente();
         cliente.setNome("Tiago");
         cliente.setCpf(12312312312L);
         cliente.setEndereco("Rua X");
         cliente.setContato(11999999999L);
-
         iClienteService.salvar(cliente);
+    }
+
+    @Test
+    public void buscarCliente(){
         Cliente clienteEncontrado = iClienteService.buscarPorId(cliente.getCpf());
 
         Assert.assertNotNull(clienteEncontrado);
-        System.out.println(clienteEncontrado.getCpf());
-
     }
+
 
 }
